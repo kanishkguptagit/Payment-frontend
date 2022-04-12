75b54920862ca304fb7600fbdf83c3973fbd149a
kanishkguptagit <1905902@kiit.ac.in>
2022-04-12 23:42:03 +0530

    button

--- src/Components/buttons/PredictButton.js
+++ src/Components/buttons/PredictButton.js
@@ -1,16 +1,19 @@
-import './AddButton.css';
+import "./AddButton.css";
 import Button from "@mui/material/Button";
 
-const PredictButton = () =>{
-    const handleClickOpen = () =>{
-        
-    }
+const PredictButton = () => {
+    const handleClickOpen = () => {};
     return (
         <>
-            <Button className="add_button"  variant="contained" onClick={handleClickOpen} style={{ height: 30,width: 150,color: "white"}}>
+            <Button
+                className="add_button"
+                variant="contained"
+                onClick={handleClickOpen}
+                style={{ height: 30, width: 150, color: "white" }}
+            >
                 Predict
             </Button>
         </>
-    )
-}
-export default PredictButton
+    );
+};
+export default PredictButton;
