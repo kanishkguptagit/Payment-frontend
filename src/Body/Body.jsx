716d6ad822cfb2651b7c5e20382f1cc72bc6534a
kanishkguptagit <1905902@kiit.ac.in>
2022-04-13 17:46:10 +0530

    added footer

--- src/Body/Body.jsx
+++ src/Body/Body.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Operations from "./Operations";
 import MainGrid from "./MainGrid";
+import Footer from "../Components/Footer";
 const Body = () => {
     const [data, setData] = useState([]);
     const [selectedRow, setSelectedRow] = useState([]);
@@ -28,6 +29,7 @@ const Body = () => {
                 reload={reload}
                 setReload={setReload}
             />
+            <Footer />
         </>
     );
 };
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,14 +1,18 @@
 import React from "react";
-import '../css/footer.css';
+import classes from './Footer.module.css';
 const Footer = () => {
     return (
-<footer className="footer">
-<div >
-<p style={{ color: "white", textAlign: "center"}}> <a href="" class="active" style={{color: 'blue' }}>Privacy Policy</a> 
-© HighRadius Corporation 2022. All rights reserved.
-</p>
- </div>
- </footer>
-);
+        <footer className={classes.footer}>
+            <div>
+                <p style={{ color: "white", textAlign: "center" }}>
+                    {" "}
+                    <a href="" class="active" style={{ color: "blue" }}>
+                        Privacy Policy
+                    </a>
+                    © HighRadius Corporation 2022. All rights reserved.
+                </p>
+            </div>
+        </footer>
+    );
 };
 export default Footer;
