350ee9b97b83564a6754cef564c065dddf9e835c
kanishkguptagit <1905902@kiit.ac.in>
2022-04-13 18:20:56 +0530

    added abc products

--- src/Title/Highradius.jsx
+++ src/Title/Highradius.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import { Toolbar } from "@mui/material";
 
 import hrc from "../hrc.svg";
+import abc from "../abc.png";
 
 import classes from './Highradius.module.css'
 
 const Highradius = () => {
     return (
-        <div className={classes.pos}>
-            <Toolbar>
-                <img className={classes.imgclass} src={hrc} alt="hrclogo"/>
+        <div>
+            <Toolbar style={{backgroundColor:"#2d4250"}}>
+                <div className={classes.pos1} style={{ width:"100%"}}>
+                <img className={classes.imgclass}  src={abc} alt="hrclogo"/>
+                </div>
+                <div className={classes.pos} style={{ width:"100%"}}>
+                <img className={classes.imgclass}  src={hrc} alt="hrclogo"/>
+                </div>
             </Toolbar>
         </div>
     )
