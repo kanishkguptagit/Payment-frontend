4467e522271193afbaaa0fe74d68968cc6324e64
kanishkguptagit <1905902@kiit.ac.in>
2022-04-14 12:14:47 +0530

    added footer to proper place

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Title from './Title/Title';
 import Body from './Body/Body';
+import Footer from './Footer/Footer';
 
 function App() {
   return (
       <>
         <Title />  
-        <Body />  
+        <Body /> 
+        <Footer /> 
       </>
   );
 }
--- src/Body/Body.jsx
+++ src/Body/Body.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import Operations from "./Operations";
 import MainGrid from "./MainGrid";
-import Footer from "../Components/Footer";
 const Body = () => {
     const [data, setData] = useState([]);
     const [selectedRow, setSelectedRow] = useState([]);
@@ -33,8 +32,7 @@ const Body = () => {
                 setReload={setReload}
                 countRows={countRows}
                 setCountRows={setCountRows}
-            />
-            <Footer />
+            />            
         </>
     );
 };
